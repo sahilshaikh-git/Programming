9dcc3cc66f7473e6d076b65a28032367bdeed031
sahilshaikh2023 <sahilshaikh6922@gmail.com>
2026-07-13 17:24:45 +0530

    update java file

---
+++ C/Competitive/Assignments/program97.java
@@ -0,0 +1,37 @@
+////////////////////////////////////////////////////////
+//
+//     Assignment 20 1 - write the program  to print number from   N down to 1  in reverse order    
+//     queation 2
+//
+//
+///////////////////////////////////////////////////////
+// 
+ 
+import java.util.*;
+
+class Logic
+{
+       void printreverse ( int num)
+       {
+              int i ; 
+              
+              for(i =num ; i>=1; i--)
+              {
+                          System.out.println(i ) ;
+              }
+                    
+             
+
+       }
+
+}
+
+ class program97
+{
+       public static void main( String A[])
+       {
+              Logic obj = new Logic();
+              obj.printreverse(10);
+       }
+}
+
