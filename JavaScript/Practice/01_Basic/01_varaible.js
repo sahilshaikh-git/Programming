9b58fb747b01f832809bd4640554506995c55c2d
sahilshaikh2023 <sahilshaikh6922@gmail.com>
2026-07-21 13:34:24 +0530

    update js

---
+++ JavaScript/Practice/01_Basic/01_varaible.js
@@ -0,0 +1,26 @@
+const account_ID  = 12345;
+
+let accountEmail = "hello@google.com";
+var accountPassword = "32535";
+
+accountCity = "Pune ";
+
+// account_ID = 223;                                                                              constant cannot change
+
+accountEmail= "raj@tum.com";
+accountPassword = "4333";
+ let accoutState ; 
+
+
+/*
+              we not using the var keyword this is old technique
+              prefer not to use the var 
+              because the scope variable
+
+*/
+console.log( account_ID);
+
+console.table([account_ID, accountEmail, accountPassword ,accountCity, accoutState]);
+
+
+
---
+++ JavaScript/Practice/01_Basic/02_datatype.js
@@ -0,0 +1,20 @@
+"use strict"  // use code are new verion of javascript
+
+              // alert(3+4);              we are using the node js not web browser 
+
+console.log( 3 + 
+       4                                                /// code readiility is high 
+)
+
+
+
+// number = 2 to the power 52
+//     bigInt  
+//    string
+//     null
+//     undefined
+//     boolean  true and false
+//     cgit
+
+
+console.log("Sahil")
