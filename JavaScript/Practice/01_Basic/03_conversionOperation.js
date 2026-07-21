891044953e8ef79676f1350d713ae471fb528bcc
sahilshaikh2023 <sahilshaikh6922@gmail.com>
2026-07-21 14:55:05 +0530

    conversionOperation

---
+++ JavaScript/Practice/01_Basic/03_conversionOperation.js
@@ -0,0 +1,40 @@
+// let score =  "33aab" 
+// let score =  "33aab" 
+
+// let score =  null ; 
+// let score =  undefined ; 
+let score =  1  ; 
+
+
+// console.log(score) ;
+// console.log(typeof score) ;
+// console.log(typeof (score)) ;
+
+let valueinNumber = Number( score);
+
+
+// console.log( typeof valueinNumber);
+// console.log(valueinNumber);
+
+
+// "33 " => 33
+//  "33afbb" => NaN
+//      "String"  => NaN
+//     "undefined" => NaN 
+//     "null" => 0 
+//     "true" => 1 
+//     "false" => 0 
+
+let isLoggedin =  "true"; 
+
+let  isBoolean = Boolean(isLoggedin);
+
+console.log(typeof (isBoolean));
+console.log(isBoolean);
+
+
+let num = " 33 ";
+
+let toString  =  String(num);
+
+console.log( typeof toString);
