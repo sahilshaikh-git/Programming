ee3c8bc3920450480713e308e9157d450ea2f296
sahil shaikh <sahilshaikh9514@gmail.com>
2026-08-20 11:24:11 +0530

    27

---
+++ C/Competitive/Assignments/program132.c
@@ -0,0 +1,57 @@
+//  assignent 26 2.   Accept number of rows and number of columns from user and display below pattern.
+//
+//            $      *      *      *         *
+//            #      $      *      *        *
+//            #      #      $      *       *
+//            #      #      #      $      *
+//            #      #      #      #     $
+
+#include<stdio.h>
+
+
+void Pattern ( int iRow  , int iCol)
+{
+
+       int i = 0 ;
+       int j = 0 ;
+
+       for( i =1  ; i <=iRow ; i++ )
+       {
+              
+                   for( j=1 ; j<=i; j++ )
+               {
+                     if(i==j)
+                     {
+                            printf("$");
+                     }
+
+                     if( i<j)
+                     {
+                            printf("#");
+                     }
+                     else
+                     {
+                            printf("*");
+                     }
+
+      
+               }
+
+               printf("\n");
+       }
+}
+int main()
+{
+       int iValue1 =0 , iValue2 = 0;
+
+       printf("Enter the Row and Column\n");
+
+       scanf(" %d %d"  , &iValue1 ,&iValue2);
+
+       Pattern(iValue1 , iValue2);
+       
+        
+       return 0 ;
+}
+
+
